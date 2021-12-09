3a57921d839f659d2455247809456543c40f92d1
chevonnesmellie <95383005+chevonnesmellie@users.noreply.github.com>
2021-12-08 19:36:12 -0500

    shows when weather last updated

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let day = date.getDay();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return `Last updated ${days[day]} ${hours}:${minutes}`;
+}
+
 function displayWeather(response) {
   console.log(response.data);
   document.querySelector("#city").innerHTML = response.data.name;
@@ -11,6 +34,9 @@ function displayWeather(response) {
     "#humidity"
   ).innerHTML = `${response.data.main.humidity}%`;
   document.querySelector("#wind").innerHTML = `${response.data.wind.speed}km/h`;
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
 }
 
 let apiKey = "35edd9e7e8e2b546aad3ee8914df1a70";
