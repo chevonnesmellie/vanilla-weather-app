f4e326cf754bd0d6fbd1d9e84ce4e827b30b96c4
chevonnesmellie <95383005+chevonnesmellie@users.noreply.github.com>
2021-12-08 19:15:00 -0500

    API added to show current weather conditions

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+function displayWeather(response) {
+  console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#current-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector(
+    "#condition"
+  ).innerHTML = ` ${response.data.weather[0].description}`;
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `${response.data.main.humidity}%`;
+  document.querySelector("#wind").innerHTML = `${response.data.wind.speed}km/h`;
+}
 
+let apiKey = "35edd9e7e8e2b546aad3ee8914df1a70";
+let city = "Toronto";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayWeather);
