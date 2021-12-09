63ab2251f5e1831d97048b65fb80c7e76e9eb722
chevonnesmellie <95383005+chevonnesmellie@users.noreply.github.com>
2021-12-08 20:31:17 -0500

    Added weather icon from API for todays weather

--- src/app.js
+++ src/app.js
@@ -34,13 +34,22 @@ function displayWeather(response) {
     "#humidity"
   ).innerHTML = `${response.data.main.humidity}%`;
   document.querySelector("#wind").innerHTML = `${response.data.wind.speed}km/h`;
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document
+    .querySelector("#icon")
+    .setAttribute("alt", response.data.weather[0].description);
   document.querySelector("#date").innerHTML = formatDate(
     response.data.dt * 1000
   );
 }
 
 let apiKey = "35edd9e7e8e2b546aad3ee8914df1a70";
-let city = "Kingston";
+let city = "Toronto";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayWeather);
