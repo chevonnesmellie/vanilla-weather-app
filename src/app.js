dde09d0047f7e054983ec1ba46032a32b25d781d
chevonnesmellie <95383005+chevonnesmellie@users.noreply.github.com>
2021-12-13 18:24:09 -0500

    called another API for forecast data

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+function getForecast(coordinates) {
+  let apiKey = "35edd9e7e8e2b546aad3ee8914df1a70";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let day = date.getDay();
@@ -45,6 +51,8 @@ function displayWeather(response) {
   document.querySelector("#date").innerHTML = formatDate(
     response.data.dt * 1000
   );
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -76,7 +84,7 @@ function displayCelsiusTemp(event) {
     Math.round(celsiusTemperature);
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row forecast-row" id="forecast">`;
@@ -114,4 +122,3 @@ celsiusLink.addEventListener("click", displayCelsiusTemp);
 let celsiusTemperature = null;
 
 search("Toronto");
-displayForecast();
